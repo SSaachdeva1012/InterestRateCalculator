513a9b9aafc3b594b999efa5bedfbcc20ac40255
SSaachdeva1012 <43412842+SSaachdeva1012@users.noreply.github.com>
2019-03-20 20:34:36 +0000

    First Commit

---
+++ InterestCalculator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace InterestCalculator
+{
+    public static class  MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Amount");
+            var amoutString = Console.ReadLine();
+
+            var rate = 0m;
+            if(decimal.TryParse(amoutString,out  var amount))
+            {
+                rate = GetInterestRate(amount);
+            }
+            var interestAmount = ConvertRateToMoney(amount, rate);
+
+            Console.WriteLine($"Amount: £{amount}");
+            Console.WriteLine($"Interest Rate: {rate}%");
+            Console.WriteLine("Interest Amount: £"+decimal.Round(interestAmount, 2, MidpointRounding.AwayFromZero));
+            Console.ReadLine();
+        }
+
+        public static decimal ConvertRateToMoney(decimal amount, decimal rate)
+        {
+            var multipiler = rate / 100;
+            return multipiler * amount;
+        }
+
+        public static decimal GetInterestRate(decimal amount)
+        {
+            var rate = 0m;
+            if (amount < 1000)
+            {
+                rate = 1;
+            }
+            else if (amount < 5000)
+            {
+                rate = 1.5m;
+            }
+            else if (amount < 10000)
+            {
+                rate = 2;
+            }
+            else if (amount < 50000)
+            {
+                rate = 2.5m;
+            }
+            else if (amount >= 50000)
+            {
+                rate = 3;
+            }
+            return rate;
+        }
+    }
+}
---
+++ InterestCalculatorTests/Test.cs
@@ -0,0 +1,178 @@
+﻿using NUnit.Framework;
+using System;
+using InterestCalculator;
+namespace InterestCalculatorTests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void Test_Amount_999_99_InterestRate_1_ok()
+        { 
+            var amount = 999.99m;
+            var expectedRate = 1m;
+            var expectedAmount = 10;
+
+            var rate = MainClass.GetInterestRate(amount);
+            var interestAmount = MainClass.ConvertRateToMoney(amount, rate);
+
+            Assert.AreEqual(expectedRate, rate);
+            Assert.AreEqual(expectedAmount, decimal.Round(interestAmount, 2, MidpointRounding.AwayFromZero));
+        }
+
+        [Test()]
+        public void Test_Amount_1000_InterestRate_1_5_ok()
+        {
+            var amount = 1000.00m;
+            var expectedRate = 1.5m;
+            var expectedAmount = 15.00;
+
+            var rate = MainClass.GetInterestRate(amount);
+            var interestAmount = MainClass.ConvertRateToMoney(amount, rate);
+
+            Assert.AreEqual(expectedRate, rate);
+            Assert.AreEqual(expectedAmount, decimal.Round(interestAmount, 2, MidpointRounding.AwayFromZero));
+        }
+
+        [Test()]
+        public void Test_Amount_1001_InterestRate_1_5_ok()
+        {
+            var amount = 1001.00m;
+            var expectedRate = 1.5m;
+            var expectedAmount = 15.02;
+
+            var rate = MainClass.GetInterestRate(amount);
+            var interestAmount = MainClass.ConvertRateToMoney(amount, rate);
+
+            Assert.AreEqual(expectedRate, rate);
+            Assert.AreEqual(expectedAmount, decimal.Round(interestAmount, 2, MidpointRounding.AwayFromZero));
+        }
+
+        [Test()]
+        public void Test_Amount_4999_99_InterestRate_1_5_ok()
+        {
+            var amount = 4999.99m;
+            var expectedRate = 1.5m;
+            var expectedAmount = 75;
+
+            var rate = MainClass.GetInterestRate(amount);
+            var interestAmount = MainClass.ConvertRateToMoney(amount, rate);
+
+            Assert.AreEqual(expectedRate, rate);
+            Assert.AreEqual(expectedAmount, decimal.Round(interestAmount, 2, MidpointRounding.AwayFromZero));
+        }
+
+        [Test()]
+        public void Test_Amount_5000_InterestRate_2_ok()
+        {
+            var amount = 5000m;
+            var expectedRate = 2m;
+            var expectedAmount = 100;
+
+            var rate = MainClass.GetInterestRate(amount);
+            var interestAmount = MainClass.ConvertRateToMoney(amount, rate);
+
+            Assert.AreEqual(expectedRate, rate);
+            Assert.AreEqual(expectedAmount, decimal.Round(interestAmount, 2, MidpointRounding.AwayFromZero));
+        }
+
+        [Test()]
+        public void Test_Amount_5000_01_InterestRate_2_ok()
+        {
+            var amount = 5000.01m;
+            var expectedRate = 2m;
+            var expectedAmount = 100;
+
+            var rate = MainClass.GetInterestRate(amount);
+            var interestAmount = MainClass.ConvertRateToMoney(amount, rate);
+
+            Assert.AreEqual(expectedRate, rate);
+            Assert.AreEqual(expectedAmount, decimal.Round(interestAmount, 2, MidpointRounding.AwayFromZero));
+        }
+
+        [Test()]
+        public void Test_Amount_9999_99_InterestRate_2_ok()
+        {
+            var amount = 9999.99m;
+            var expectedRate = 2m;
+            var expectedAmount = 200;
+
+            var rate = MainClass.GetInterestRate(amount);
+            var interestAmount = MainClass.ConvertRateToMoney(amount, rate);
+
+            Assert.AreEqual(expectedRate, rate);
+            Assert.AreEqual(expectedAmount, decimal.Round(interestAmount, 2, MidpointRounding.AwayFromZero));
+        }
+
+        [Test()]
+        public void Test_Amount_10000_InterestRate_2_5_ok()
+        {
+            var amount = 10000m;
+            var expectedRate = 2.5m;
+            var expectedAmount = 250;
+
+            var rate = MainClass.GetInterestRate(amount);
+            var interestAmount = MainClass.ConvertRateToMoney(amount, rate);
+
+            Assert.AreEqual(expectedRate, rate);
+            Assert.AreEqual(expectedAmount, decimal.Round(interestAmount, 2, MidpointRounding.AwayFromZero));
+        }
+
+
+        [Test()]
+        public void Test_Amount_10001_InterestRate_2_5_ok()
+        {
+            var amount = 10001m;
+            var expectedRate = 2.5m;
+            var expectedAmount = 250.03;
+
+            var rate = MainClass.GetInterestRate(amount);
+            var interestAmount = MainClass.ConvertRateToMoney(amount, rate);
+
+            Assert.AreEqual(expectedRate, rate);
+            Assert.AreEqual(expectedAmount, decimal.Round(interestAmount, 2, MidpointRounding.AwayFromZero));
+        }
+
+        [Test()]
+        public void Test_Amount_49999_99_InterestRate_2_5_ok()
+        {
+            var amount = 49999.99m;
+            var expectedRate = 2.5m;
+            var expectedAmount = 1250;
+
+            var rate = MainClass.GetInterestRate(amount);
+            var interestAmount = MainClass.ConvertRateToMoney(amount, rate);
+
+            Assert.AreEqual(expectedRate, rate);
+            Assert.AreEqual(expectedAmount, decimal.Round(interestAmount, 2, MidpointRounding.AwayFromZero));
+        }
+
+        [Test()]
+        public void Test_Amount_50000_InterestRate_3_ok()
+        {
+            var amount = 50000m;
+            var expectedRate =3m;
+            var expectedAmount = 1500;
+
+            var rate = MainClass.GetInterestRate(amount);
+            var interestAmount = MainClass.ConvertRateToMoney(amount, rate);
+
+            Assert.AreEqual(expectedRate, rate);
+            Assert.AreEqual(expectedAmount, decimal.Round(interestAmount, 2, MidpointRounding.AwayFromZero));
+        }
+
+        [Test()]
+        public void Test_Amount_50001_InterestRate_3_ok()
+        {
+            var amount = 50001m;
+            var expectedRate = 3m;
+            var expectedAmount = 1500.03;
+
+            var rate = MainClass.GetInterestRate(amount);
+            var interestAmount = MainClass.ConvertRateToMoney(amount, rate);
+
+            Assert.AreEqual(expectedRate, rate);
+            Assert.AreEqual(expectedAmount, decimal.Round(interestAmount, 2, MidpointRounding.AwayFromZero));
+        }
+    }
+}
